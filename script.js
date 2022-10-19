0e037aa9eb53c112268955166ccbb3f2fd7e1ae8
Risiks <arthur.geersen@gmail.com>
2022-10-19 13:22:15 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const chargement = document.querySelector('.chargement');
+
+function HTMLinXHR() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=PcVQebW8dytUuElmEiS9zubacfcZ2ouI6BWUnCzz');
+    xhr.send();
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+            console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            chargement.style.display = 'display';
+            const data = JSON.parse(xhr.response);
+            const img = document.createElement('img');
+            const h1 = document.createElement('h1');
+            const h2 = document.createElement('h2');
+            const h3 = document.createElement('h4');
+            img.src = data.url;
+            h1.textContent = data.title;
+            h2.textContent = data.date;
+            h3.textContent = data.explanation;
+            document.body.appendChild(h1);
+            document.body.appendChild(h2);
+            document.body.appendChild(h3);
+            document.body.appendChild(img);
+            chargement.style.display = 'none';
+        }
+    };
+    xhr.onerror = function () {
+        console.log("Request failed");
+    };
+}
+
+window.addEventListener('load', HTMLinXHR);
+console.dir(HTMLinXHR)
